38d2d7b99a6c1f8f1e3a6b2125cbc2089d3a0529
Your Name <rimonron.ad@gmail.com>
2024-09-26 13:25:06 +0600

    navbar done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        work: ['Work Sans', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ]
 }
 
