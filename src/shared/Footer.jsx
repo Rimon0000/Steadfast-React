53d0bb0f6aeceebc3c113272fd50da90abc05946
Your Name <rimonron.ad@gmail.com>
2024-09-28 21:15:03 +0600

    up to date

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <div className="mt-12 bg-[#111111] text-[#CFCFCF] font-work">
-          <footer className=" px-9 py-[61.5px]">
+          <footer className=" px-[37px] pt-[44px] pb-[63px]">
             <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4">
               <div>
                 {/* <img className="pb-2" src="" alt=""> */}
@@ -16,9 +16,9 @@ const Footer = () => {
                 <h3 className="pb-3 text-[28px] font-medium">Useful Link</h3>
                 <div className="text-base">
                   <p>Homes</p>
-                  <p className="py-3">Service</p>
+                  <p className="py-4">Service</p>
                   <p>Cases</p>
-                  <p className="py-3">Blogs</p>
+                  <p className="py-4">Blogs</p>
                   <p>Contact Us</p>
                 </div>
               </div>
@@ -28,7 +28,7 @@ const Footer = () => {
                 <div className="text-base">
                   <p>555 4th 5t NW, Washington<br />
                     DC 20530, United States</p>
-                  <p className="py-3">+88 01750 000 000<br />
+                  <p className="py-4">+88 01750 000 000<br />
                     +88 01750 000 000</p>
                   <p>info@gmail.com
                     info@gmail.com</p>
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar px-[50px] py-[10px] font-work">
+        <div className="navbar pl-[44px] pr-[44px] py-[15.5px] font-work place-content-center mx-auto">
           <div className="navbar-start">
             <div className="dropdown">
               <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -31,19 +31,19 @@ const Navbar = () => {
               </div>
               <ul
                 tabIndex={0}
-                className=" menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-[15px] font-semibold text-slate-500">
+                className=" menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-[16px] font-semibold text-black">
                 {navOptions}
               </ul>
             </div>
-            <a className="btn btn-ghost text-[21px] font-medium">SteadFast</a>
+            <a className="text-[24px] mb-[10px] text-black font-bold">SteadFast</a>
           </div>
           <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1 text-[15px] font-semibold text-slate-500">
+            <ul className="menu menu-horizontal px-1 text-[16px] font-semibold text-black">
               {navOptions}
             </ul>
           </div>
           <div className="navbar-end">
-            <button className="btn btn-secondary bg-[#6c757d] border-[#6c757d] text-[#fff] btn-sm px-4 py-5 text-base font-light place-content-center">Login</button>
+            <a className="btn btn-secondary bg-[#6c757d] border-[#6c757d] text-[#fff] btn-sm px-[11px] py-[17px] mb-[2px] text-base font-light place-content-center rounded-md">Login</a>
           </div>
         </div>
     );
